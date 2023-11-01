5605c7ee44442339fc85cf0aade3a4e68a8da684
Krishna Panthi <krishnarho@gmail.com>
2023-11-01 17:07:15 +0545

    Installing React and Tailwind CSS using Vite

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="m-5 bg-green-600 text-white">This is install using Vite</h1>
+    </div>
+  )
+}

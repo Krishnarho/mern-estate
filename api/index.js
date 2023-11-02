184afe2a28f563560e259e20c9371f689b92b519
Krishna Panthi <krishnarho@gmail.com>
2023-11-02 14:45:35 +0545

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const connectToMongo = require('./db');
+const express = require('express');
+
+connectToMongo();
+const app = express();
+const port = 5000;
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`MERN-estate app listening on port ${port}`)
+});
+
